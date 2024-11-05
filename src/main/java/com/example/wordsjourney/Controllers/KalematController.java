9571fa7a45fc9205374025019d9728c6168cb0aa
Mazen Ahmed <mazen20190441@gmail.com>
2024-11-05 17:05:48 +0200

    Updated the 'Update' Api

--- src/main/java/com/example/wordsjourney/Controllers/KalematController.java
+++ src/main/java/com/example/wordsjourney/Controllers/KalematController.java
@@ -55,7 +55,7 @@ public class KalematController {
         }
         }
     }
-    @PutMapping("/update")
+    @PatchMapping("/update")
     public ResponseEntity<GeneralResponse<userDTO>> Update(
             @RequestHeader(value = "email") String email,
             @RequestHeader(value = "name") String name,
